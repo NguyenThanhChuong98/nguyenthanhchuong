1e3a7f35a3fd69102a1d26729fe9ccae47befe6f
NguyenThanhChuong98 <54970862+NguyenThanhChuong98@users.noreply.github.com>
2020-08-03 17:02:04 +0700

    Add files via upload

--- baitap.py
+++ baitap.py
@@ -1,5 +1,9 @@
-file_name = '1500000 Sales Records.csv'
-lines = (line for line in open(file_name))
-list_line = (s.rstrip().split(",") for s in lines)
-cols = next(list_line)
-print(cols)
+def read_file():
+    file_name = "20 records sale.csv"
+    n = open(file_name, "r")
+    for lines in n:
+        print(lines)
+    n.close()
+
+if __name__ == "__main__":
+    read_file()
