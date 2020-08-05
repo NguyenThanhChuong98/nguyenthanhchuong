2fd87a8d914cc7c2938be20f1b8fbc0971eddea4
NguyenThanhChuong98 <54970862+NguyenThanhChuong98@users.noreply.github.com>
2020-08-05 21:10:55 +0700

    Add files via upload

--- baitap.py
+++ baitap.py
@@ -1,9 +1,36 @@
-def read_file():
-    file_name = "20 records sale.csv"
-    n = open(file_name, "r")
-    for lines in n:
-        print(lines)
-    n.close()
+import re
+from slugify import slugify
 
-if __name__ == "__main__":
-    read_file()
+
+def read_first_N_line(file_name, N):
+    with open(file_name) as file:
+        for line in(file.readlines()[:N]):
+            print(line)
+
+
+def read_last_N_line(file_name, N):
+    with open(file_name) as file:
+        for line in (file.readlines()[-N:]):
+            print(line)
+
+
+def search_by_words(file_name, n):
+    with open(file_name) as file:
+        x = slugify(n)
+        print(x)
+        for line in (file.readlines()):
+            y = slugify(line)
+            if x in y:
+                print(line)
+            else:
+                pass
+
+
+if __name__ == '__main__':
+    file_name = "20 records sale.csv"
+    N = 5
+    n = input("Input your words: ")
+    try:
+        search_by_words(file_name, n)
+    except:
+        print('File not found')
