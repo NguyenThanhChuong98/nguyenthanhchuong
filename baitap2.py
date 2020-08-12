508c454b0a7f6c38dd8d57a0696f528f8381c801
NguyenThanhChuong98 <54970862+NguyenThanhChuong98@users.noreply.github.com>
2020-08-12 10:45:41 +0700

    Add files via upload

--- baitap2.py
+++ baitap2.py
@@ -1,14 +1,13 @@
 import time
-import threading
 
 
-def count_region_group_1(thread_id,delay):
+def count_region_group_1():
     list1 = []
     with open(file_name) as file:
         for row in file:
             list1.append(row.split(',')[0])
         n = [[x, list1.count(x)] for x in list1]
-        print(n)
+    	print(n)
     file.close()
 
 
@@ -24,11 +23,6 @@ def count_region_group_2():
 
 if __name__ == "__main__":
     file_name = "20 records sale.csv"
-    t1 = threading.Thread(target=count_region_group_1,args=(1,3))
-    t2 = threading.Thread(target=count_region_group_1,args=(2,4))
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
     start_time = time.time()
+    count_region_group_1()
     print("Time Executed: %s seconds" % (time.time() - start_time))
