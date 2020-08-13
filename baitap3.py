66210c7f8eb1531e899a5e901df21a59d55b44a4
NguyenThanhChuong98 <54970862+NguyenThanhChuong98@users.noreply.github.com>
2020-08-13 10:29:32 +0700

    Add files via upload

--- baitap3.py
+++ baitap3.py
@@ -1,53 +1,31 @@
-list1 = []
-
-
 def sort_total_revenue():
-    global list1
-    with open(file_name) as file:
-        for line in file.readlines()[1:]:
-            list1.append(line.split(','))
-        for i in range(len(list1[-3])):
-            j = i - 1
-            nxt_element = list1[i]
-            while (list1[j] > nxt_element) and (j >= 0):
-                list1[j + 1] = list1[j]
-                j = j - 1
-            list1[j + 1] = nxt_element
-        print('\n'.join(map(str, list1)))
-
+	sort_list(-3)
 
 def sort_total_cost():
-    global list1
-    with open(file_name) as file:
-        for line in file.readlines()[1:]:
-            list1.append(line.split(','))
-        for i in range(len(list1[-2])):
-            j = i - 1
-            nxt_element = list1[i]
-            while (list1[j] > nxt_element) and (j >= 0):
-                list1[j + 1] = list1[j]
-                j = j - 1
-            list1[j + 1] = nxt_element
-        print('\n'.join(map(str, list1)))
-
+	sort_list(-2)
 
 def sort_total_profit():
-    global list1
+	sort_list(-1)
+
+def sort_list(n):
+    list1 = []
     with open(file_name) as file:
-        for line in file.readlines()[1:]:
-            list1.append(line.split(','))
-        for i in range(len(list1)[-1]):
-            j = i - 1
-            nxt_element = list1[i]
-            while (list1[j] > nxt_element) and (j >= 0):
-                list1[j + 1] = list1[j]
-                j = j - 1
-            list1[j + 1] = nxt_element
-        print('\n'.join(map(str, list1)))
+        for row in file.readlines()[1:]:
+            list1.append(row.split(','))
+        for i in range(len(list1[n]) - 1):
+            for j in range(len(list1[n]) - i - 1):
+                if list1[j] > list1[j + 1]:
+                    list1[j], list1[j + 1] = list1[j + 1], list1[j]
+        print('\n\n'.join(map(str, list1)))
 
-
+        # for i in range(len(list1[])):
+        #     j = i - 1
+        #     nxt_element = list1[i]
+        #     while (list1[j] > nxt_element) and (j >= 0):
+        #         list1[j + 1] = list1[j]
+        #         j = j - 1
+        #     list1[j + 1] = nxt_element
+        # print('\n\n'.join(map(str, list1)))
 if __name__ == "__main__":
     file_name = "20 records sale.csv"
     sort_total_revenue()
-    sort_total_cost()
-    sort_total_profit()
